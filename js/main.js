58d772368ee61a66a1e756702cd338629f502daf
eeordonezz <respositorio50@gmail.com>
2024-07-31 10:19:50 -0600

    Creacion de la base para trabajar
    Se creo la base, donde se posicionar cada elemento de este sitio web, y se han vinculado archivos para trabajar de forma mas ordenada

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let vinculo = document.querySelector(".root");
+
+vinculo.innerHTML =`
+
+<header>ee </header>
+
+<div class="categorias">ss </div>
+
+
+<div class="placa_madre"> 
+
+
+<div class="base_madre"></div>
+
+</div>
+
+<footer> ff</footer>
+
+
+`;
+
